5362544dc1778de3bae7a46d9949f9601222b64c
Tiago <tiagopereicx@gmail.com>
2021-12-29 10:48:59 -0300

    erro.php: aplicação do tema na página de erro

--- themes/cafecontrol/error.php
+++ themes/cafecontrol/error.php
@@ -1,3 +1,4 @@
+<?php $v->layout("theme") ?>
 <!--404-->
 <article class="not_found">
     <div class="container content">
@@ -6,11 +7,8 @@
             <h1>Ooops. Essa página não existe :/</h1>
             <p>Sentimos muito, mas o conteúdo que você tentou acessar não existe, está indisponível ou foi removido
                 :/</p>
-            <a class="not_found_btn gradient gradient-green gradient-hover transition radius" title="home" href="./">Continue
+            <a class="not_found_btn gradient gradient-green gradient-hover transition radius" title="home" href="<?= url() ?>">Continue
                 navegando</a>
         </header>
     </div>
 </article>
-
-<!--OPT OUT-->
-<?php require __DIR__ . "/optout.php"; ?>
